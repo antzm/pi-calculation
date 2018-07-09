7fc8ea25e02b005d98e36b4ec419b41fc97951c6
antzm <36490615+antzm@users.noreply.github.com>
2018-07-09 14:47:16 +0300

    Update pi.js

--- pi.js
+++ pi.js
@@ -1,3 +1,8 @@
+// A script to calculate Pi (3.14)
+// using one of the simplest infinte series
+// sum = 1/(1*1) + 1/(2*2) + 1/(3*3) + 1/(4*4) + 1/(5*5)... infinte terms
+// and where Pi = sqrt(sum*6)
+
 console.time('Total time for Pi calculation: ');
 let n=0, sum=0;
 for (let i=1; i<1000000; i++) {
@@ -6,3 +11,4 @@ for (let i=1; i<1000000; i++) {
 };
 console.log("pi approximation = " + Math.sqrt(sum*6));
 console.timeEnd('Total time for Pi calculation: ');
+
